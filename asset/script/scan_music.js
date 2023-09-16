6ab9f0c749f0cd5f4913dcff5bd4dc09eb266120
TokisakiNinoVn <trieuduytancbg@gmail.com>
2023-09-16 16:12:43 +0700

    New update

--- asset/script/scan_music.js
+++ asset/script/scan_music.js
@@ -51,6 +51,25 @@ for (let i = 0; i < 17; i++) {
 }
 
 
+const html_top100 = `
+<div class="item_quare item_quare-big">
+<div class="div_img .div_img-big">
+    <a href="">
+        <img src="./asset/images/Ai_1x1_eye.jpg" alt="">
+        <div class="div_icon--in--img">
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="play-circle-outline"></ion-icon>
+            <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+        </div>              
+    </a>
+</div>
+<p class="decrip_item">Yaosobi</p>
+<a class="singer-name" href="">Hoshino Ai</a>
+</div> `
+const top100 = document.getElementById('all_item-top100');
+for (let i = 0; i < 5; i++) {
+    top100.innerHTML += html_top100;
+}
 
 
 
@@ -70,6 +89,6 @@ const radio_item = `<div class="item_circle radio-one-item">
 </div> ;`
 
 const radio_music = document.getElementById('all-radio-item');
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 7; i++) {
     radio_music.innerHTML += radio_item;
 }
