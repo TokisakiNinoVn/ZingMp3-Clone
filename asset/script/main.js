8b0a67039ff1cf4425fd824fe6c61bda5012e06d
TokisakiNinoVn <trieuduytancbg@gmail.com>
2023-09-17 14:43:03 +0700

    Update header

--- asset/script/main.js
+++ asset/script/main.js
@@ -47,11 +47,11 @@ clickRightBtn.addEventListener("click", () => {
 
 
 
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
-document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-        e.preventDefault();
-    }
-});
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// });
+// document.addEventListener("keydown", function(e) {
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+//         e.preventDefault();
+//     }
+// });
