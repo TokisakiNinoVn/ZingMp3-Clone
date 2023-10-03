e588d09dd30a1a33852dcde1c4f062390467cecc
tokisakininovn <trieuduytancbg@gmail.com>
2023-10-03 22:05:52 +0700

    New Update

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    alert('Trang không có responsive, đề cử kích thước màn hình [1530:995]');
-    alert('Album: Hoshino Ai.\n Github: TokisakiNinoVn.');
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     alert('Trang không có responsive, đề cử kích thước màn hình [1530:995]');
+//     alert('Album: Hoshino Ai.\n Github: TokisakiNinoVn.');
+// });
 
 
 const link_img = [
@@ -15,6 +15,7 @@ const link_img = [
         url: "./asset/images/Ai_hori3.jpg",
     },
 ];
+
 const bannerImgs = document.querySelectorAll(".banner_img");
 let currentIndex = 0;
 
@@ -36,23 +37,25 @@ const clickRightBtn = document.getElementById("click-right");
 
 // Thêm event listener cho nút click-left để chuyển đến ảnh trước đó
 clickLeftBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + link_img.length) % link_img.length;
+    // currentIndex = (currentIndex - 1 + link_img.length) % link_img.length;
+    currentIndex = (currentIndex + 1) % link_img.length ;
+
     changeImage();
 });
 
 // Thêm event listener cho nút click-right để chuyển đến ảnh kế tiếp
 clickRightBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % link_img.length;
+    currentIndex = (currentIndex + 1) % link_img.length ;
     changeImage();
 });
 
 
 
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
-document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-        e.preventDefault();
-    }
-});
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// });
+// document.addEventListener("keydown", function(e) {
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+//         e.preventDefault();
+//     }
+// });
